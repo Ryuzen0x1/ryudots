189ef41f3b16f9266aaa2ce969dbff2ae3ea1d10
Shaon Samanta <57990129+Ryuzen0x1@users.noreply.github.com>
2025-05-15 23:38:48 +0530

    Polkit auth code changed with nix in mind

---
+++ Configs/.local/lib/qu4ntum/polkitauth.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# /* ---- 󱝁 https://github.com/JaKooLit 󱝁 ---- */  ##
+# This is for polkits, it will start from top and will stop if the top is executed
+
+# Polkit possible paths files to check
+
+# Use different directory on NixOS
+if [ -d /run/current-system/sw/libexec ]; then
+    libDir=/run/current-system/sw/libexec
+else
+    libDir=/usr/lib
+fi
+
+${libDir}/polkit-gnome/polkit-gnome-authentication-agent-1 &
